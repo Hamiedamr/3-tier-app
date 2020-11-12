41689e752819699e86344f4a5b889ac9d483aeb4
Hamied Amr <abdelhamied.amr@outlook.com>
2020-11-12 03:00:07 +0200

    run shell correctly

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-gnome-terminal --tab  -- javac 'src/Server.java'
-gnome-terminal --tab  -- javac 'src/Intermediate.java'
-gnome-terminal --tab  -- javac 'src/Client.java'
+javac  'src/Server.java' 'src/Intermediate.java' 'src/Client.java' 'src/Intermediate.java' 'src/IntermediateHandler.java' 'src/ClientHandler.java'
+cd 'src'
+gnome-terminal --title="Station" --tab -- java Server 
+gnome-terminal --title="Computing" --tab -- java Intermediate 
+gnome-terminal --title="Decision" --tab -- java Client
 
-gnome-terminal --tab --working-directory='./src' java Server
-gnome-terminal --tab --working-directory='./src' java Intermediate
-gnome-terminal --tab --working-directory='./src' java Client
+
+# gnome-terminal --tab  --working-directory='./src' -- javac Client.java
+
+# gnome-terminal --tab --working-directory='./src' -- java Server
+# gnome-terminal --tab --working-directory='./src' -- java Intermediate
+# gnome-terminal --tab --working-directory='./src' -- java Client
 
 
 
