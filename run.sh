c2d960deb191ac64e953ed170d1720813d902880
Hamied Amr <abdelhamied.amr@outlook.com>
2020-11-12 03:01:21 +0200

    run shell comments

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+#Compile
 javac  'src/Server.java' 'src/Intermediate.java' 'src/Client.java' 'src/Intermediate.java' 'src/IntermediateHandler.java' 'src/ClientHandler.java'
 cd 'src'
+#Run the app
 gnome-terminal --title="Station" --tab -- java Server 
 gnome-terminal --title="Computing" --tab -- java Intermediate 
 gnome-terminal --title="Decision" --tab -- java Client
