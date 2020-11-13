47f68f4d4762050d7494819c364522ff07495fd7
Hamied Amr <abdelhamied.amr@outlook.com>
2020-11-13 02:31:41 +0200

    v1.0.0

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#Installations of java
+sudo apt update
+sudo apt install default-jdk
+echo java -version
+sudo apt install openjdk-8-jdk
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:linuxuprising/java
+sudo apt update
+sudo apt install oracle-java11-installer
+sudo update-alternatives --config java
 #Compile
 javac  'src/Server.java' 'src/Intermediate.java' 'src/Client.java' 'src/Intermediate.java' 'src/IntermediateHandler.java' 'src/ClientHandler.java' 'src/ServerHandler.java'
 cd 'src'
